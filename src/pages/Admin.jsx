a50cde3b2a667ac390d6964aa1c831094d64bf9c
amishamulani0415 <129196758+amishamulani0415@users.noreply.github.com>
2025-08-03 01:01:25 +0530

    "Deleted duplicate file after moving frontend to client/"

--- src/pages/Admin.jsx
+++
@@ -1,4 +0,0 @@
-function Admin() {
-  return <h1 className="text-center text-3xl mt-10">🛠 Admin Panel</h1>;
-}
-export default Admin;
--- src/pages/Home.jsx
+++
@@ -1,4 +0,0 @@
-function Home() {
-  return <h1 className="text-center text-3xl mt-10">🏠 Home Page</h1>;
-}
-export default Home;
