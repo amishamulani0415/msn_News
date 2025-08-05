14c95b649305a57d9f4acac3fd13c39dc9cdbc82
Dakshish <dakshjetwa3@gmail.com>
2025-08-05 23:31:21 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/server': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/server/, '/api/v1'),
+      },
+    },
+  },
   plugins: [react()],
 })
