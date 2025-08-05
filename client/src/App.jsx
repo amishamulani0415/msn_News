073fdbe8cab66643599b43ee2d5b0237ad0dc672
Dakshish <dakshjetwa3@gmail.com>
2025-08-05 23:34:54 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,9 @@ import Admin from "./pages/Admin";
 import EditNews from "./pages/EditNews";
 import AdminLogin from "./pages/adminLogin";
 import AdminPanel from "./pages/adminPanel";
+import Login from "./pages/login.jsx";
+import Register from "./pages/register.jsx";
+import Dashboard from "./pages/dashboard.jsx";
 
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
         <Route path="/edit/:id" element={<EditNews />} />
         <Route path='/admin' element={<AdminLogin />} />
         <Route path='/admin/panel' element={<AdminPanel />} />
+        <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </Router>
   );
