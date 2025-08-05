450e94eb185adf363823ad9bb4187d63b23f0dbf
Dakshish <dakshjetwa3@gmail.com>
2025-08-05 23:32:16 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
 import EditNews from "./pages/EditNews";
+import AdminLogin from "./pages/adminLogin";
+import AdminPanel from "./pages/adminPanel";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/edit/:id" element={<EditNews />} />
+        <Route path='/admin' element={<AdminLogin />} />
+        <Route path='/admin/panel' element={<AdminPanel />} />
       </Routes>
     </Router>
   );
